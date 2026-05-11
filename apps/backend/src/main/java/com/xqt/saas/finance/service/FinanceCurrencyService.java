1e8346a21bbe19c7986501f5ba8335eae175968c
ctykk <ctyKK@outlook.com>
2026-05-11 11:12:25 +0800

    修复 pageCurrencyWithExchangeRate 分页问题

--- apps/backend/src/main/java/com/xqt/saas/finance/service/FinanceCurrencyService.java
+++ apps/backend/src/main/java/com/xqt/saas/finance/service/FinanceCurrencyService.java
@@ -1,6 +1,7 @@
 package com.xqt.saas.finance.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xqt.saas.finance.dto.request.FinanceCurrencySaveRequest;
@@ -10,13 +11,13 @@ import com.xqt.saas.finance.entity.FinanceCurrencyExchangeRateHistoryType;
 import com.xqt.saas.finance.entity.FinanceCurrencyType;
 import com.xqt.saas.finance.mapper.FinanceCurrencyExchangeRateHistoryMapper;
 import com.xqt.saas.finance.mapper.FinanceCurrencyMapper;
+import jakarta.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -44,17 +45,16 @@ public class FinanceCurrencyService extends ServiceImpl<FinanceCurrencyMapper, F
         removeById(id);
     }
 
-    // fixme: 换成 IPage<FinanceCurrencyWithExchangeRateView>
-    public List<FinanceCurrencyWithExchangeRateView> pageCurrencyWithExchangeRate(Long pageNum, Long pageSize) {
+    public IPage<FinanceCurrencyWithExchangeRateView> pageCurrencyWithExchangeRate(Long pageNum, Long pageSize) {
         // 先获取到该分页的 codes，根据 code 查询对应的应收、应付
         var currencies = page(pageNum, pageSize);
 
-        var res = new ArrayList<FinanceCurrencyWithExchangeRateView>();
-        for (var currency : currencies) {
+        var records = new ArrayList<FinanceCurrencyWithExchangeRateView>();
+        for (var currency : currencies.getRecords()) {
             var fromHistory = getLatestRate(currency.getCode(), "应收");
             var toHistory = getLatestRate(currency.getCode(), "应付");
 
-            res.add(new FinanceCurrencyWithExchangeRateView(
+            records.add(new FinanceCurrencyWithExchangeRateView(
                     currency.getId(),
                     currency.getCode(),
                     currency.getName(),
@@ -64,20 +64,24 @@ public class FinanceCurrencyService extends ServiceImpl<FinanceCurrencyMapper, F
                     toHistory == null ? null : toHistory.getEffectiveFrom()
             ));
         }
+        var result = new Page<FinanceCurrencyWithExchangeRateView>();
+        result.setRecords(records);
+        result.setTotal(currencies.getRecords().size());
+        result.setSize(records.size());
+        result.setCurrent(currencies.getCurrent());
 
-        return res;
+        return result;
     }
 
-    private List<FinanceCurrencyType> page(Long pageNum, Long pageSize) {
-        var res = currencyMapper.selectPage(
+    private IPage<FinanceCurrencyType> page(Long pageNum, Long pageSize) {
+        return currencyMapper.selectPage(
                 new Page<>(pageNum, pageSize),
                 new LambdaQueryWrapper<FinanceCurrencyType>().
                         orderByDesc(FinanceCurrencyType::getCreatedAt)
         );
-        return res.getRecords();
     }
 
-    private FinanceCurrencyExchangeRateHistoryType getLatestRate(String code, String scenario) {
+    private @Nullable FinanceCurrencyExchangeRateHistoryType getLatestRate(String code, String scenario) {
         var wrapper = new LambdaQueryWrapper<FinanceCurrencyExchangeRateHistoryType>();
         wrapper.eq(FinanceCurrencyExchangeRateHistoryType::getCode, code).
                 eq(FinanceCurrencyExchangeRateHistoryType::getApplicationScenario, scenario).
