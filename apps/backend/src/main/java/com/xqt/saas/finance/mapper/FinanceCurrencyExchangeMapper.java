6a955fbfe57c868b4a29fb7825089186b0b4fc3e
ctykk <ctyKK@outlook.com>
2026-05-11 12:12:26 +0800

    补充 mapper 注解

--- apps/backend/src/main/java/com/xqt/saas/finance/mapper/FinanceCurrencyExchangeMapper.java
+++ apps/backend/src/main/java/com/xqt/saas/finance/mapper/FinanceCurrencyExchangeMapper.java
@@ -2,6 +2,8 @@ package com.xqt.saas.finance.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.xqt.saas.finance.entity.FinanceCurrencyExchangeType;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface FinanceCurrencyExchangeMapper extends BaseMapper<FinanceCurrencyExchangeType> {
 }
